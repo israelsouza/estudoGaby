9aa5af95552e8cce7599d34280ed27839d6215ad
sofia <sofiafamorim@gmail.com>
2025-08-28 17:28:51 -0300

    refactor: mudança na sessão 'historia' para 'voce sabia'

--- src/components/HistorySection.tsx
+++ src/components/HistorySection.tsx
@@ -5,18 +5,20 @@ function HistorySection() {
         <div>
           <div>
             <h3 className="text-3xl font-bold text-foreground mb-4 text-center">
-              Nossa História
+              Você sabia?
             </h3>
             <p className="text-muted-foreground mb-4">
-              Fundado em 1648, o Exército Brasileiro possui uma rica tradição de
-              servir ao país. Ao longo dos séculos, participou de importantes
-              momentos da história nacional, desde a independência até os dias
-              atuais.
+            O militar do exército é o profissional que tem a função de proteger o país de ameaças internas ou externas, 
+            dessa maneira promovendo e mantendo a paz. Para isso, é submetido a um treinamento intenso que irá prepará-lo
+             para as mais diversas situações e como agir diante das mesmas.
+             É enviado para missões e operações, cada uma com seus objetivos específicos e grau de dificuldade que fará com 
+               que esse profissional desenvolva as competências necessárias para exercer a sua atividade. 
             </p>
             <p className="text-muted-foreground">
-              Hoje, é uma força moderna e bem equipada, com mais de 220.000
-              militares, distribuídos em todo o território nacional, sempre
-              prontos para defender o Brasil e contribuir para a paz mundial.
+              A carreira militar expõe o indivíduo a situações constantes de estresse, ansiedade e pressão, exigindo preparo físico e
+               mental resiliente. O desgaste pode gerar o Estresse Ocupacional, um distúrbio causado pela falta de habilidades para 
+               enfrentar a deterioração da função. 
+               
             </p>
           </div>
           <div className="grid grid-cols-2 gap-4 text-center pt-8">
