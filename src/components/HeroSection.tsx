8a712a3e460863760fd08a15616fb04323ef8644
Israel de Souza <israellimas@hotmail.com>
2025-08-30 21:41:35 -0300

    modifica o fundo da imagem principal pela nova e altera o texto de apresentação

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
-import heroImage from "@/assets/hero-military.jpg";
+import heroImage from "@/assets/exercito.png";
 import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <section className="relative bg-gradient-to-r from-military-green to-military-green-light text-white min-h-[80vh] flex items-center">
+    <section className="relative bg-gradient-to-r from-military-green to-military-green-light text-white min-h-[80vh] flex items-center heroSection">
       <div className="absolute inset-0 opacity-20">
         <img 
           src={heroImage} 
@@ -15,14 +15,16 @@ const HeroSection = () => {
       
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 z-10">
         <div className="max-w-3xl">
-          <h1 className="text-4xl md:text-6xl font-bold mb-6 leading-tight">
-            Servindo o Brasil com
-            <span className="block text-green-200">Honor e Dedicação</span>
+          <h1 className="text-4xl md:text-4xl font-bold mb-6 leading-tight">
+              Tecnologia e gestão: como funciona a área de administração no comando militar do sudeste - II Exército
+            <span className="block text-green-200">
+            </span>
           </h1>
+
           
           <p className="text-xl md:text-2xl mb-8 text-green-100 leading-relaxed">
-            Conheça a história, missão e valores do Exército Brasileiro. 
-            Uma instituição centenária dedicada à defesa da pátria e do povo brasileiro.
+            Site academico com o intuito de divulgar a pesquisa realizada pela estudante Gabriela Orsi estudante de Gestão de Recursos Humanos da Fatec
+              Zona Leste.
           </p>
           
           <div className="flex flex-col sm:flex-row gap-4 heroSection__container-btn">
